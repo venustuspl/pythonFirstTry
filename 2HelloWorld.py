8cc9c1bd5894f62df42d2150eab664f7b5fa8736
venustuspl <venustus.pl@gmail.com>
2022-12-08 18:22:26 +0100

    2HelloWorld.py

---
+++ 2HelloWorld.py
@@ -0,0 +1,16 @@
+print('HelloWorld')
+
+t=[1,2,3,4,5,'nietoperz']
+print(t)
+
+print('1/3='+str(1/3))
+
+x=123
+y=67
+print('x={}, y={}'.format(x,y))
+
+x=123
+y=67
+print('x={}, y={}'.format(x,y))
+
+print(f'x={x}') #konwersja do string
