693098b478913f407d9043a93f9958b058a1c2c7
venustuspl <venustus.pl@gmail.com>
2022-12-29 20:10:59 +0100

    20221229Set.py

---
+++ 20221229Set.py
@@ -0,0 +1,20 @@
+z = {}
+z2 = set()
+
+z3 = {1, 3, 2, 1, 5, 1}
+print(z3)
+
+z4 = {(1, "A"), (2, "B"), (1, "C"), (1, "A")}
+print(z4)
+
+lista = [1, 2, 3, 4, 3, 2, 1]
+zestaw = set(lista)
+lista2 = list(zestaw)
+print(lista2)
+s = {1, 2, 3, 4}
+s.add(5)
+s.remove(1)
+print(s)
+
+s.clear()
+print(s)
