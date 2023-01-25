b3b9c95518ec5a7a5ac1c25262a0d13d387aaa7a
Tomasz Kostro <tomasz.kostro@marketplanet.pl>
2023-01-25 07:20:58 +0100

    20230124changeExtensions.py refactor

--- 20230124changeExtensions.py
+++ 20230124changeExtensions.py
@@ -2,8 +2,9 @@ import glob, os
 from datetime import datetime
 path = os.path.abspath(os.getcwd())
 os.path.abspath(path)
-now = datetime.now().strftime("%Y%b%d")
+now = datetime.now().strftime("%Y%b%d_%H_%M_%S")
 
 for file in glob.glob("*.xml"):
     os.rename(file, file + "Q" + now)
     print(file)
+
