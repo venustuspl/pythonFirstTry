7a155c06dc07181159f1e1522443befbeec0453a
venustuspl <venustus.pl@gmail.com>
2023-02-15 17:28:10 +0100

    20230215RegexChar.py

---
+++ 20230215RegexChar.py
@@ -0,0 +1,21 @@
+#\d - cyfry
+#\D - znaki nie będące cyfrą
+#\w  - alfanumeryki
+#\W - wszystko co nie jest alfanumerykiem
+#\s - białe znaki
+#\S - wszystko co nie jest białym znakiem
+#. - dowolny znak
+#[a-f] - małe litery z zakresu a-f
+#[A-F] - duże litery z zakresu A-F
+#[0-3] - cyfry w zakresie 0-3
+#[a-z0-6] - małe litery w zakresie a-z lub cyfry w zakresie 0-6
+#[^a-f] - wszystko co nie jest małą literą w zakresie a-f
+
+
+#3ilosc
+#* - dowolna ilość znaków - w tym zero!
+#+ - co najmniej jedno wystąpienie, ale może być też więcej
+#? - zero lub jedno wystąpienie - ale nie więcej
+#{1,5} - od jednego do pięciu wystąpień
+#{5,} - co najmniej 5 wystąpień
+#{,5} - do 5 wystąpień
