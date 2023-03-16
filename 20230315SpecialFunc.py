851d1c4ce3209749ff151f5f7cc8660e4c52c7c1
venustuspl <venustus.pl@gmail.com>
2023-03-16 15:44:33 +0100

    20230315SpecialFunc.py add getitem setitem

--- 20230315SpecialFunc.py
+++ 20230315SpecialFunc.py
@@ -13,3 +13,37 @@ class Samochod:
 s = Samochod("Renault","Megane")
 print(s.marka,s.model)
 print(s)
+
+#__add__ dodanie 2 obiektów
+class Samochod:
+    marka=None
+    model=None
+    def __add__(self, other):
+        return "Kraksa 2 Sebastianów w dresie. Jeden jechał {}, drugi {}".format(self,other)
+    def __init__(self,marka,model):
+        self.marka=marka
+        self.model=model
+    def __str__(self):
+        return "marka={} model={}".format(self.marka,self.model)
+
+s=Samochod("Audi","A4")
+print(s)
+s2=Samochod("Bmw","e46")
+s3=s+s2
+print(s3)
+
+#__getitem__ i __setitem__
+class MyDictionary:
+    dict=dict()
+    def __setitem__(self, key, value):
+        self.dict[key]=value
+    def __getitem__(self, key):
+        return self.dict[key]
+
+md = MyDictionary()
+md['klucz']='wartość'
+md['klucz1']='wartość1'
+md['klucz2']='wartość2'
+print(md['klucz'])
+print(md['klucz1'])
+print(md['klucz2'])
