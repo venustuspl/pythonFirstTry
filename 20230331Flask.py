87e8a7bee68cf05ec47a660f6d84d3582b83356f
venustuspl <venustus.pl@gmail.com>
2023-04-07 18:58:16 +0200

    20230407render_template

--- 20230331Flask.py
+++ 20230331Flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def hello():
     return '<h1>Hello, World!</h1>'
 
+
+
+#Python has a built-in variable called __name__ that records the name of the currently running module or script.
+
+@app.route('/index')
+def hello_template():
+    return render_template("index.html")
+
 if __name__ == '__main__':
-    app.run();
+    app.run(debug=True, port=80);
